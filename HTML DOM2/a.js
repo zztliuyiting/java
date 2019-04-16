c5623f526685eef87fdd9a1c44c023efe32dfe17
zztliuyiting <43942330+zztliuyiting@users.noreply.github.com>
2019-04-16 11:25:04 +0800

    html dom2

---
+++ HTML DOM2/a.js
@@ -0,0 +1,5 @@
+function init(){
+  var divtag=document.getElementById('lyt')
+  divtag.setAttribute('style','height:150px; width:500px; background-color:pink; margin-left:300px')
+}
+window.onload=init
